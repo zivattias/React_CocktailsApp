42dd08a2fbc857143e45330c2f3b39920055833a
zivattias <ziv.attias7@gmail.com>
2023-03-02 15:32:13 +0200

    Navbar component

--- cocktails/src/App.tsx
+++ cocktails/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
-    <p>Cocktails App</p>
+    <Navbar></Navbar>
   );
 }
 
