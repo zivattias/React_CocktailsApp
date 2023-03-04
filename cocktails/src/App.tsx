40c4a835cb97f3457fee2152b56c6d72ed26e402
zivattias <ziv.attias7@gmail.com>
2023-03-04 19:07:09 +0200

    reconfig

--- cocktails/src/App.tsx
+++ cocktails/src/App.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Section from "./components/Section/Section";
+import Hero from "./features/Hero/Hero";
 
-import './App.css';
-import Navbar from './components/Navbar/Navbar';
-
-function App() {
-  return (
-    <Navbar></Navbar>
-  );
+function App(): JSX.Element {
+    return (
+        <>
+            <Navbar></Navbar>
+            <Section name="hero">
+                <Hero></Hero>
+            </Section>
+        </>
+    );
 }
 
 export default App;
